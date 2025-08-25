d26913e38b2baadfec6ed64b0f06859d5525ea57
alejo-heras <alejandro.gonzalez.heras@gmail.com>
2025-08-26 01:19:53 +0200

    Updated

--- R/FW-metricas-padron.R
+++ R/FW-metricas-padron.R
@@ -2,6 +2,7 @@
 #' @param padron data.frame con flag_pre, flag_long, (opcional) flag_post y last_long
 #' @param cutoff_days días de inactividad para abandono (por defecto 7)
 #' @return data.frame con métricas y conteos
+#' @export
 metricas_padron <- function(padron, cutoff_days = 7) {
   pre  <- as.logical(padron$flag_pre);  pre[is.na(pre)]   <- FALSE
   long <- as.logical(padron$flag_long); long[is.na(long)] <- FALSE
